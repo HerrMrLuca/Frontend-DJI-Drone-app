746382068398700a04e44077284c45815c2217e2
HerrMrLuca <92095094+HerrMrLuca@users.noreply.github.com>
2022-12-06 23:14:42 +0100

    Added all files
    Added all files

---
+++ src/api/http/config.ts
@@ -0,0 +1,36 @@
+export const CURRENT_CONFIG = {
+
+  // license
+  appId: '124442', // You need to go to the development website to apply.
+  appKey: 'ff629d8e9901bd0096f1c8ab109cc97', // You need to go to the development website to apply.
+  appLicense: 'hxv4Wi1BtG0/0ZUIgi7gTYPO+yJzy+RC6NXYEyKemq3pOSAWEbs4UzcQ8edMNIRmQ/C8YIOOW0PdP00M2cMn2F0kElayYNS1jZebPBjXur75WUnjbTE+KePwpWLTOL56CfgkOSA++NgV8brFV+krL7i4tiTJ8hN7JA178UoUtj4=', // You need to go to the development website to apply.
+
+  // http
+  baseURL: 'http://10.29.18.198:6789/', // This url must end with "/". Example: 'http://192.168.1.1:6789/'
+  websocketURL: 'ws://10.29.18.198:6789/api/v1/ws', // Example: 'ws://192.168.1.1:6789/api/v1/ws'
+
+  // livestreaming
+  // RTMP  Note: This IP is the address of the streaming server. If you want to see livestream on web page, you need to convert the RTMP stream to WebRTC stream.
+  rtmpURL: 'rtmp://192.240.105.123/live/', // Example: 'rtmp://192.168.1.1/live/'
+  // GB28181 Note:If you don't know what these parameters mean, you can go to Pilot2 and select the GB28181 page in the cloud platform. Where the parameters same as these parameters.
+  gbServerIp: 'Please enter the server ip.',
+  gbServerPort: 'Please enter the server port.',
+  gbServerId: 'Please enter the server id.',
+  gbAgentId: 'Please enter the agent id',
+  gbPassword: 'Please enter the agent password',
+  gbAgentPort: 'Please enter the local port.',
+  gbAgentChannel: 'Please enter the channel.',
+  // RTSP
+  rtspUserName: 'Please enter the username.',
+  rtspPassword: 'Please enter the password.',
+  rtspPort: '8554',
+  // Agora
+  agoraAPPID: '81151cf17c8641c18bf00e453ccaede4',
+  agoraToken: '007eJxTYNhnLnNnytcV/Grmb5an3T1kKr2Nz7+clSdo/SXB/mS3o3YKDBaGhqaGyWmG5skWZiaGyYYWSWkGBqkmpsbJyYmpKakmwbPLkxsCGRksl3UzMzJAIIjPxZCUmJuUmVKUn5fKwAAAmWogTA==',
+  agoraChannel: 'bambidrone',
+
+  // map
+  // You can apply on the AMap website.
+  amapKey: 'Please enter the amap key.',
+
+}
