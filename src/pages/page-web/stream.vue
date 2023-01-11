62ed2d68cbe70d003658a9efbd4f88a5b6fff6fb
TalhaD007 <92798213+TalhaD007@users.noreply.github.com>
2023-01-11 23:18:29 +0100

    added some logs

--- src/pages/page-web/stream.vue
+++ src/pages/page-web/stream.vue
@@ -9,13 +9,116 @@
 
 <script lang="ts" setup>
 import LivestreamAgora from '/@/components/livestream-agora.vue'
-import { ref } from 'vue';
-import { ELocalStorageKey } from '/@/types';
-import { getDeviceTopo, getLiveCapacity, getPlatformInfo, getUserInfo } from '/@/api/manage';
+import { reactive, ref } from 'vue'
+import { ELocalStorageKey } from '/@/types'
+import { getDeviceTopo, getLiveCapacity, getPlatformInfo, getUserInfo } from '/@/api/manage'
 
 /* region testing get-functions and content */
 
 const workspaceId = ref(localStorage.getItem(ELocalStorageKey.WorkspaceId)!)
+const userId = ref(localStorage.getItem(ELocalStorageKey.UserId)!)
+const device = ref(localStorage.getItem(ELocalStorageKey.Device)!)
+
+const state = reactive({
+  currentType: '',
+  coverIndex: 0
+})
+
+/* fuck i have to figure this shit out somehow
+
+const deviceInfo = reactive({
+  gateway: {
+    capacity_percent: str,
+    transmission_signal_quality: str,
+  } as GatewayOsd,
+  dock: {
+    media_file_detail: {
+      remain_upload: 0
+    },
+    sdr: {
+      up_quality: str,
+      down_quality: str,
+      frequency_band: -1,
+    },
+    network_state: {
+      type: 0,
+      quality: 0,
+      rate: 0,
+    },
+    drone_in_dock: 0,
+    drone_charge_state: {
+      state: 0,
+      capacity_percent: str,
+    },
+    rainfall: str,
+    wind_speed: str,
+    environment_temperature: str,
+    environment_humidity: str,
+    temperature: str,
+    humidity: str,
+    job_number: 0,
+    acc_time: 0,
+    first_power_on: 0,
+    positionState: {
+      gps_number: str,
+      is_fixed: 0,
+      rtk_number: str,
+      is_calibration: 0,
+      quality: 0,
+    },
+    storage: {
+      total: 0,
+      used: 0,
+    },
+    electric_supply_voltage: 0,
+    working_voltage: str,
+    working_current: str,
+    backup_battery_voltage: 0,
+    mode_code: -1,
+    cover_state: -1,
+    supplement_light_state: -1,
+    putter_state: -1,
+
+  } as DockOsd,
+  device: {
+    gear: -1,
+    mode_code: EModeCode.Disconnected,
+    height: str,
+    home_distance: str,
+    horizontal_speed: str,
+    vertical_speed: str,
+    wind_speed: str,
+    wind_direction: str,
+    elevation: str,
+    position_state: {
+      gps_number: str,
+      is_fixed: 0,
+      rtk_number: str
+    },
+    battery: {
+      capacity_percent: str,
+      landing_power: str,
+      remain_flight_time: 0,
+      return_home_power: str,
+    },
+    latitude: 0,
+    longitude: 0,
+  } as DeviceOsd
+})
+
+ */
+
+console.log('start workspaceId log')
+console.log(workspaceId.value)
+console.log('end workspaceId log')
+
+console.log('start userId log')
+console.log(userId.value)
+console.log('end userId log')
+
+console.log('start device log')
+console.log(device.value)
+console.log('end device log')
 
 console.log('start getDeviceTopo')
 const test1 = getDeviceTopo(workspaceId.value)
